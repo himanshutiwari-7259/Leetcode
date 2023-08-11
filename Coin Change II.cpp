6cc5c9df3fc5cfece3a0283cac45d58a47607562
Himanshu Tiwari <96172956+himanshutiwari-7259@users.noreply.github.com>
2023-08-11 22:05:05 +0530

    Create Coin Change II.cpp
    In the "Coin Change II" problem, we are given coins of various denominations and a specific amount. Our task is to determine the number of different ways we can make up that amount using the coins. Unlike other coin change problems where we need to minimize or maximize the number of coins, here we need to find the total number of possible combinations.
    
    For instance, with coins [1,2,5] and amount 5, we have the following combinations:
    
    1. 5 = 5
    2. 5 = 2 + 2 + 1
    3. 5 = 2 + 1 + 1 + 1
    4. 5 = 1 + 1 + 1 + 1 + 1
    Hence, there are 4 ways to make up the amount 5 using the coins [1,2,5].

---
+++ Coin Change II.cpp
@@ -0,0 +1,17 @@
+//Using a 1D Dynamic Programming Array
+
+Class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
+    }
+};
